9b9db9909aa38bf8efbea5a39d88251c8a9ef09e
Tom Kadwill <tomkadwill@gmail.com>
2016-11-25 06:48:19 +0000

    redirect_uri should be configurable per environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :fitbit,
            ENV['FITBIT_OAUTH_CLIENT_ID'],
            ENV['FITBIT_OAUTH_CLIENT_SECRET'],
-           scope: 'profile heartrate', redirect_uri: 'http://localhost:3000/auth/fitbit/callback'
+           scope: 'profile heartrate', redirect_uri: ENV['FITBIT_CALLBACK_URL']
 end
