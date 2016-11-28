5b5ca68f62169c84fa1aaa88de7dda23db884929
Tom Kadwill <tomkadwill@gmail.com>
2016-11-28 19:13:22 +0000

    Tidy up classes into smaller methods

--- app/models/heart_rate_data.rb
+++ app/models/heart_rate_data.rb
@@ -3,6 +3,22 @@ class HeartRateData < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
   def update_heart_rate_data!
+    update!(data: heart_rate_data_for_the_day)
+  end
+
+  private
+
+  def heart_rate_data_for_the_day
+    client.heart_rate_intraday_time_series(
+      user_id: '-',
+      date: 'today',
+      start_time: '00:00',
+      end_date: '24:00',
+      detail_level: '1min'
+    )
+  end
+
+  def client
     client = Fitbit::Client.new(
       client_id: ENV['FITBIT_OAUTH_CLIENT_ID'],
       client_secret: ENV['FITBIT_OAUTH_CLIENT_SECRET'],
@@ -10,7 +26,5 @@ class HeartRateData < ApplicationRecord
       refresh_token: user.refresh_token,
       expires_at: user.token_expires_at
     )
-
-    self.data = client.heart_rate_intraday_time_series(user_id: '-', date: 'today', start_time: '00:00', end_date: '24:00', detail_level: '1min')
   end
 end
--- spec/models/heart_rate_data_spec.rb
+++ spec/models/heart_rate_data_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 RSpec.describe HeartRateData, type: :model do
 
   let(:user) { FactoryGirl.create(:user) }
-  let(:heart_rate_data) { described_class.new(user: user) }
+  let(:heart_rate_data) { described_class.create!(user: user) }
 
   describe "#update_heart_rate_data!" do
     it "updates heart rate data, for a user" do
       heart_rate_data.update_heart_rate_data!
 
-      expect(heart_rate_data.data).to be_truthy
+      expect(heart_rate_data.reload.data).to be_truthy
     end
   end
 end
