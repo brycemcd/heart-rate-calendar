0933d03e329b3c002f47f5cdf306e31c2e162fb9
Tom Kadwill <tomkadwill@gmail.com>
2016-12-01 19:03:09 +0000

    Update update_heart_rate_data! to accept an (optional) date param in the format YYYY-mm-dd

--- app/models/heart_rate_data.rb
+++ app/models/heart_rate_data.rb
@@ -1,16 +1,21 @@
 class HeartRateData < ApplicationRecord
   belongs_to :user, dependent: :destroy
 
-  def update_heart_rate_data!
-    update!(data: heart_rate_data_for_the_day)
+  def update_heart_rate_data!(date: 'today')
+    data = heart_rate_data_for_the_day(date)
+    return false unless data
+
+    update!(data: data)
   end
 
   private
 
-  def heart_rate_data_for_the_day
+  def heart_rate_data_for_the_day(date)
+    return false unless valid_date?(date)
+
     client.heart_rate_intraday_time_series(
       user_id: '-',
-      date: 'today',
+      date: date,
       start_time: '00:00',
       end_date: '24:00',
       detail_level: '1min'
@@ -25,5 +30,12 @@ class HeartRateData < ApplicationRecord
       refresh_token: user.refresh_token,
       expires_at: user.token_expires_at
     )
+  end
+
+  def valid_date?(date)
+    return true if date == 'today'
+
+    raise ArgumentError, 'invalid date' unless date =~ /\d{4}-\d{2}-\d{2}/
+    Date.parse(date)
   end
 end
--- spec/models/heart_rate_data_spec.rb
+++ spec/models/heart_rate_data_spec.rb
@@ -6,9 +6,39 @@ RSpec.describe HeartRateData, type: :model do
 
   describe '#update_heart_rate_data!' do
     it 'updates heart rate data, for a user' do
-      heart_rate_data.update_heart_rate_data!
+      VCR.use_cassette("heart rate data today") do
+        heart_rate_data.update_heart_rate_data!
 
-      expect(heart_rate_data.reload.data).to be_truthy
+        expect(heart_rate_data.reload.data).to be_truthy
+      end
+    end
+
+    it "updates heart rate data, for a user, at specific date" do
+      VCR.use_cassette("heart rate data 2016-07-29") do
+        heart_rate_data.update_heart_rate_data!(date: '2016-07-29')
+
+        expect(heart_rate_data.reload.data).to be_truthy
+      end
+    end
+
+    describe "invalid date" do
+      it "does not update heart rate data, for a user" do
+        expect do
+          heart_rate_data.update_heart_rate_data!(date: '20160729')
+        end.to raise_error(ArgumentError, 'invalid date')
+      end
+
+      it "does not update heart rate data, for a user" do
+        expect do
+          heart_rate_data.update_heart_rate_data!(date: '29-07-2016')
+        end.to raise_error(ArgumentError, 'invalid date')
+      end
+
+      it "does not update heart rate data, for a user" do
+        expect do
+          heart_rate_data.update_heart_rate_data!(date: '2016-07-99')
+        end.to raise_error(ArgumentError, 'invalid date')
+      end
     end
   end
 end
